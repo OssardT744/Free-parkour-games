f93187c13f37c8552ec2fd2ab0d137d5b8b8df2c
UnitArk <rouvesmika@hotmail.com>
2016-03-27 17:32:48 +0200

    the physic is work

--- new_parkour.py
+++ new_parkour.py
@@ -25,8 +25,10 @@ block_saut = False
 continuer_tomber = False
 cpt_continuer_tomber_physique = 0
 act_physique = False
+continuer_act_physique = False
 
 while continuer:
+	print(window.get_at((x+20,y+41)))
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
@@ -43,13 +45,14 @@ while continuer:
 		if event.type == KEYUP:
 			if event.key == K_RIGHT or event.key == K_LEFT:
 				x_change = 0
-			if event.key == K_UP:
+			elif event.key == K_UP:
 				act_physique = True
 				temps_saut = 40
 				y_change = 2
 				act_temps_saut = False
 				touche_gris_re_saut = True
 				block_saut = True
+				continuer_act_physique = True
 	
 	#~ si le joueur a sauter le plus longtemps possible :
 	if act_temps_saut:
@@ -58,11 +61,28 @@ while continuer:
 			y_change = 2
 			block_saut = True
 			touche_gris_re_saut = True
+			continuer_act_physique = True
 			act_physique = True
 		
+	if act_temps_saut == False:
+		if block_saut == False:
+			if window.get_at((x,y+41)) == blanc and window.get_at((x+20,y+41)) == blanc and window.get_at((x+41,y+41)) == blanc:
+				block_saut = True
+				touche_gris_re_saut = True
+				y_change = 2
+				act_physique = True
+				continuer_act_physique = True
+			
 	
-	if touche_gris_re_saut == True:
-		if window.get_at((x,y+41)) == gris or window.get_at((x+20,y+41)) == gris or window.get_at((x+41,y+41)) == gris:
+	
+	if act_physique:
+		cpt_continuer_tomber_physique += 1
+		if cpt_continuer_tomber_physique == 3:
+			y_change += 1
+			cpt_continuer_tomber_physique = 0
+	
+	if window.get_at((x,y+41)) == gris or window.get_at((x+20,y+41)) == gris or window.get_at((x+41,y+41)) == gris:
+		if touche_gris_re_saut == True:
 			y_change = 0
 			block_saut = False
 			touche_gris_re_saut = False
@@ -71,19 +91,7 @@ while continuer:
 			cpt_continuer_tomber_physique = 0
 			act_temps_saut = False
 	
-	if act_temps_saut == False:
-		if window.get_at((x,y+41)) == blanc and window.get_at((x+20,y+41)) == blanc and window.get_at((x+41,y+41)) == blanc:
-			block_saut = True
-			touche_gris_re_saut = True
-			y_change = 2
-			act_physique = True
 		
-	
-	if act_physique:
-		cpt_continuer_tomber_physique += 1
-		if cpt_continuer_tomber_physique == 5:
-			y_change += 1
-			cpt_continuer_tomber_physique = 0
 	
 	x += x_change
 	y += y_change
